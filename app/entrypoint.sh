8e56a07b599fa07170d8b08538552f5696459657
mikohan <angara99@gmail.com>
2022-11-08 09:32:15 +0300

    added postgres dump

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn quora.wsgi:application  -w 4 --bind 0.0.0.0:8000
+gunicorn app.wsgi:application  -w 4 --bind 0.0.0.0:8000
